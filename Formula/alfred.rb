fba44b9014fdf3fe0814938b1f00b38d95747d8e
cryptohodlers <45209115+cryptohodlers@users.noreply.github.com>
2018-11-20 20:03:52 +0100

    Update alfred.rb

--- Formula/alfred.rb
+++ Formula/alfred.rb
@@ -1,7 +1,7 @@
 class Alfred < Formula
   desc "A user friendly Stellar wallets manager."
   homepage "https://github.com/celrenheit/alfred"
-  url "https://github.com/celrenheit/alfred/releases/download/v0.1.10/alfred_0.1.10_macOS_64-bit.tar.gz"
+  url "https://github.com/cryptohodlers/alfred/releases/download/v0.1.10/alfred_0.1.10_macOS_64-bit.tar.gz"
   version "0.1.10"
   sha256 "551f65f170e933ceaed91d512122fd73caa7e09675df5330e02c6f21e0c02303"
 
